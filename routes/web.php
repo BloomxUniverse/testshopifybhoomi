29445fd9444e39144d3ba05ee44949a3722e85f5
nischalmishrajiji <niiiiiiiiiish@gmail.com>
2024-02-22 21:54:02 +0530

    ok

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//hello
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
